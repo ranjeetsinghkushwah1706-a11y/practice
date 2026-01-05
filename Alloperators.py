16fa806e931caec5f05060ec7a0f9441ece1cd8c
RANJEET SINGH KUSHWAH <ranjeetsinghkushwah@RANJEETs-MacBook-Air.local>
2026-01-05 17:40:08 +0530

    firstcommit

---
+++ Alloperators.py
@@ -0,0 +1,106 @@
+#Arithematic operator
+a = 10
+
+b = 2
+
+#sum
+sum= a + b
+
+print("Addition of a and b ",sum)
+
+#Multiplication
+multiply=a * b
+print("multiplication of a and b ",multiply)
+
+#subtraction
+subtract= a - b
+print("subtraction of a and b ",subtract)
+
+#Division
+divide=a /b
+print("Division of a and b ",divide)
+
+#modules
+mod=a % b
+print("modules of a and b ",mod)
+
+#power
+pow=a ** b
+print("power of a and b ",pow)
+
+
+# Relational/Comprasion operator
+a = 10
+b = 5
+
+#Equall to operator
+print(a == b)   # False
+
+#Not equall to operator
+print(a != b)   # True
+
+#Greater to operator
+print(a > b)    # True
+
+#Less to operator
+print(a < b)    # False
+
+#Greater than to operator
+print(a >= b)   # True
+
+#Less than to operator
+print(a <= b)   # False
+
+
+#Assignment operator
+
+#Simple assignment
+a = 10
+print(a)
+
+#Add and assign
+a += 5
+print(a)   
+
+#Subtract and assign
+a -= 3
+print(a)   
+
+#Multiply and assign
+a *= 2
+print(a) 
+
+#Divide and assign
+a /= 4
+print(a)   
+
+#Modulus and assign
+a %= 5
+print(a)   
+
+#Floor divide and assign
+a //= 2
+print(a)   
+
+#Power and assign
+a **= 3
+print(a)   # 0.0
+
+
+#logical operator
+
+
+print(not False)
+print(not True)
+
+val1 = True
+val2 = True
+val3 = False
+val4 = False
+
+print("AND operator", val1 and val2)
+print("AND operator", val1 and val3)
+print("AND operator", val3 and val4)
+print("OR operator", val1 or val2)
+print("OR operator", val1 or val3)
+print("OR operator", val3 or val4)
