b39fa738bc4c957db6bbe2651841d5ca473d5cc3
RANJEET SINGH KUSHWAH <ranjeetsinghkushwah@RANJEETs-MacBook-Air.local>
2026-01-05 18:35:28 +0530

    firstcommit

---
+++ #casting.py
@@ -0,0 +1,21 @@
+#conversion
+# Converting string to int 
+
+x = "100"
+y = int(x)
+print(y, type(y))
+
+
+#Convert int to float
+
+a = 5
+b = float(a)
+print(b, type(b))
+  
+
+# Convert number to string
+n = 50
+s = str(n)
+print(s, type(s))
+ 
+
