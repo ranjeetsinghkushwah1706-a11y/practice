f5db109e552832ffd14db1b55629fa4fc120d888
RANJEET SINGH KUSHWAH <ranjeetsinghkushwah@RANJEETs-MacBook-Air.local>
2026-01-05 18:45:04 +0530

    firstcommit

---
+++ Type_Conversion.py
@@ -0,0 +1,9 @@
+#Type Conversion (Implicit Conversion)
+#Python automatically converts one data type to another when needed.
+
+a = 10      # int
+b = 2.5     # float
+
+sum = a + b   # int + float → float
+print(sum)    # 12.5
+print(type(sum))  # <class 'float'>
