a85f60c2c9dcee0a99e6cf3cde6a0b21f207b5b6
RANJEET SINGH KUSHWAH <ranjeetsinghkushwah@RANJEETs-MacBook-Air.local>
2026-01-06 11:03:22 +0530

    secondcommit

---
+++ #Dictionary_casting.py
@@ -0,0 +1,7 @@
+#Dictionary_casting
+
+d = {"a": 1, "b": 2}
+
+print(list(d))        # keys
+print(list(d.values()))
+print(list(d.items()))
---
+++ #string_to_list_tuple_set.py
@@ -0,0 +1,11 @@
+#string_to_list_tuple_set
+
+s = "python"
+
+lst = list(s)
+tup = tuple(s)
+st = set(s)
+
+print(lst)   # ['p', 'y', 't', 'h', 'o', 'n']
+print(tup)
+print(st)
