03ac4055db5ab299e171679c9db6ea0af7fc559f
RANJEET SINGH KUSHWAH <ranjeetsinghkushwah@RANJEETs-MacBook-Air.local>
2026-01-06 12:50:24 +0530

    secondcommit

---
+++ #Slicing.py
@@ -0,0 +1,17 @@
+#Slicing
+
+#Normal Slicing
+s = "PYTHON"
+
+print(s[::2])     # PTO
+print(s[1::2])    # YHN
+print(s[::-1])    # NOHTYP (reverse)
+print(s[4:1:-1])  # OHT
+
+
+#Negative sliciing
+s = "PYTHON"
+
+print(s[-4:-1])   # THO
+print(s[-5:])     # YTHON
+print(s[:-2])     # PYTH
---
+++ #function_in_string.py
@@ -0,0 +1,30 @@
+#function_in_string
+
+#Lower / Upper
+s = "PyThOn"
+print(s.lower())
+print(s.upper())
+
+#Replace
+s = "I like Java"
+print(s.replace("Java", "Python"))
+
+#Count and Find
+s = "banana"
+print(s.count("a"))
+print(s.find("n"))
+
+#Check Type of String
+print("123".isdigit())
+print("abc".isalpha())
+print("abc123".isalnum())
+
+#Remove Spaces
+s = "  hello  "
+print(s.strip())
+
+#Reverse a String
+s = "python"
+print(s[::-1])
+
+
