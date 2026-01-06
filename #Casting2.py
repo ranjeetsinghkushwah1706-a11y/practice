8e789e113d854d7f84e612469c7cf900d1b3a97b
RANJEET SINGH KUSHWAH <ranjeetsinghkushwah@RANJEETs-MacBook-Air.local>
2026-01-06 10:56:30 +0530

    secondcommit

---
+++ #Casting2.py
@@ -0,0 +1,32 @@
+#Casting
+#different operation in casting
+
+#Addition
+num1 = int(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Sum =", num1 + num2)
+
+
+#Subtraction
+
+num1 = int(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Sub =", num1 - num2)
+
+#Multiplication 
+
+num1 = int(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("multiply=", num1 * num2)
+
+#Division
+
+num1 = int(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("div =", num1 / num2)
+
+
